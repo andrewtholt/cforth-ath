bcf668c88247c27cbb2ba20725c5031fa59195ac
andrewtholt <andrewtholt60@gmail.com>
2018-10-12 16:20:15 +0100

    Simple script to generate cfort to connect to a wifi AP.

--- build/esp32/mkConnect.sh
+++ build/esp32/mkConnect.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+WIFI_BASE=""
+WIFI_PASSWORD=""
+OP="/dev/tty"
+
+while getopts b:ho:p: val
+do
+    case $val in
+        b) WIFI_BASE="$OPTARG";;
+        h) printf "Usage -h -b <BASE NAME> -p <PASSWORD> -o <OUTPUT_FILE>\n" 
+            exit 2;;
+        o) OP="$OPTARG";;
+        p) WIFI_PASSWORD="$OPTARG";;
+        *) usage;;
+    esac
+done
+
 if [ -z "$WIFI_BASE" ]; then
     echo "Set WIFI_BASE"
     exit
@@ -11,8 +28,8 @@ if [ -z "$WIFI_PASSWORD" ]; then
 fi
 
 
-echo ":noname \" ${WIFI_BASE}\" ; to wifi-sta-ssid"
-echo ":noname \" ${WIFI_PASSWORD}\" ; to wifi-sta-password"
+echo ":noname \" ${WIFI_BASE}\" ; to wifi-sta-ssid"  > $OP
+echo ":noname \" ${WIFI_PASSWORD}\" ; to wifi-sta-password" >> $OP
 
-echo "wifi-sta-on"
+echo "wifi-sta-on" >> $OP
 
