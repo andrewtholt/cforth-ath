6071bd5075521886e72718af6c25cda07d998ba6
Andrew Holt <andrewtholt60@gmail.com>
2022-01-12 16:01:11 +0000

    rpi:extend-posix.c:Match x86 version.

--- src/app/host-serial-rpi/extend-posix.c
+++ src/app/host-serial-rpi/extend-posix.c
@@ -706,9 +706,9 @@ cell ((* const ccalls[])()) = {
 C(example1)    //c sum { i.a i.b -- i.sum }
 C(athGoodbye)    //c goodbye { i.a -- }
 C(athUname)   //c uname { -- }
-C(athHostname) //c hostname { i.len a.buffer -- i.len }
-C(athOS) //c os { i.len a.buffer -- i.len }
-C(athCPU) //c cpu { i.len a.buffer -- i.len }
+C(athHostname) //c hostname { i.len a.buffer --  }
+C(athOS) //c os { i.len a.buffer -- }
+C(athCPU) //c cpu { i.len a.buffer --  }
 C(athGetenv)   //c getenv { a.name i.len a.buffer -- i.len }
 C(athDlopen)   //c dlopen { a.name i.len i.flag -- a.lib }
 C(athDlsym)    //c dlsym  { a.name i.len i.lib -- a.func }
