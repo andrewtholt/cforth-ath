166bd5bd68c7f11859f7b3aab0a875a5b97b8f7a
Andrew (labrpi) <andrewtholt60@gmail.com>
2022-03-01 11:27:21 +0000

    Deploy script

--- build/host-serial-rpi/deploy.sh
+++ build/host-serial-rpi/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 PKG=$(ls -tr *.deb | tail -1)
+
+echo "Copying "$PKG" to repo"
 
 if [ -f "$PKG" ]; then
     scp $PKG 192.168.10.124:/var/www/debian/armhf
 fi
 
+echo "..done."
 
